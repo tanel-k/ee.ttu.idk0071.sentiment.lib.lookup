ebd259b0a73588421e0b26ae1188d84fd57026d0
Tanel Prikk <tanel.prikk@outlook.com>
2017-02-24 17:07:17 +0200

    refactored

--- src/main/java/ee/ttu/idk0071/sentiment/lib/analysis/SentimentAnalyzer.java
+++ src/main/java/ee/ttu/idk0071/sentiment/lib/analysis/SentimentAnalyzer.java
@@ -1,29 +1,7 @@
 package ee.ttu.idk0071.sentiment.lib.analysis;
 
-import java.net.MalformedURLException;
-
-import de.l3s.boilerpipe.BoilerpipeProcessingException;
-import de.l3s.boilerpipe.extractors.ArticleExtractor;
-import ee.ttu.idk0071.sentiment.lib.analysis.impl.ViveknSentimentAPI;
 import ee.ttu.idk0071.sentiment.lib.analysis.objects.PageSentiment;
-import ee.ttu.idk0071.sentiment.lib.utils.HttpUtils;
 
-public class SentimentAnalyzer {
-	public static PageSentiment analyze(String url, int timeOutMillis) throws BoilerpipeProcessingException, MalformedURLException {
-		try {
-			ArticleExtractor articleExtractor = ArticleExtractor.getInstance();
-			SentimentAPI viveknAPI = new ViveknSentimentAPI();
-			
-			// get raw HTML
-			String html = HttpUtils.getHtml(url, timeOutMillis);
-			
-			// get human-readable text from HTML
-			String text = articleExtractor.getText(html);
-			
-			// analyze text
-			return viveknAPI.getSentiment(text);
-		} catch (Throwable t) {
-			throw new RuntimeException(t);
-		}
-	}	
+public interface SentimentAnalyzer {
+	PageSentiment analyze(String url) throws SentimentAnalysisException;
 }
