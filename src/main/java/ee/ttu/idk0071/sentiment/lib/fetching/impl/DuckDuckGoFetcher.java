0707508a843ed718b9b198a334a9fb77881343f1
Tanel Prikk <tanel.prikk@outlook.com>
2017-05-02 20:04:22 +0300

    prep for more fetchers

---
+++ src/main/java/ee/ttu/idk0071/sentiment/lib/fetching/impl/DuckDuckGoFetcher.java
@@ -0,0 +1,10 @@
+package ee.ttu.idk0071.sentiment.lib.fetching.impl;
+
+import ee.ttu.idk0071.sentiment.lib.fetching.api.SearchEngineFetcher;
+
+public class DuckDuckGoFetcher extends SearchEngineFetcher {
+	public DuckDuckGoFetcher() {
+		super(0, null, null);
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+}
