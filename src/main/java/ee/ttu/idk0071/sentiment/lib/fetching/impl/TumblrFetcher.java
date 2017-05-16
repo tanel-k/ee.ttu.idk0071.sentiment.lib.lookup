c44b40194386c83472aeb0f27deaa8453a3bfbe5
Margus Tanneberg <m.tanneberg@gmail.com>
2017-05-16 21:53:43 +0300

    Tumblr fetcher done

--- src/main/java/ee/ttu/idk0071/sentiment/lib/fetching/impl/TumblrFetcher.java
+++ src/main/java/ee/ttu/idk0071/sentiment/lib/fetching/impl/TumblrFetcher.java
@@ -12,35 +12,42 @@ import ee.ttu.idk0071.sentiment.lib.utils.HTTPUtils;
 
 public class TumblrFetcher implements Fetcher {
 
-	private static final String SCROLL_STOP_VALUE = "Try another search?";
+	private static final Long USUAL_PAGE_SIZE = 10L;
 	private static final String TUMBLR_URL = "https://tumblr.com";
 	private static final String RESULT_ANCHOR_SELECTOR = ".posts div.post_body";
-	private static final String FILTER_ANCHOR_SELECTOR = "span.control_text";
-	private static final String FILTER_ITEM_ANCHOR_SELECTOR = "span.menu_item_text";
+	private static final String FILTER_ITEM_ANCHOR_SELECTOR = "i.icon_post_text_small";
 
 	@Override
 	public List<String> fetch(Query query) throws FetchException {
 		try {
+			int scrollCount = 0;
+			int SCROLL_STOP_VALUE = (int) (query.getMaxResults() / USUAL_PAGE_SIZE);
+
 			Browser browser = BrowserFactory.getBrowser();
 			String URL = getSearchPageURL(query);
 			browser.open(URL);
-			
-			//does not work so far
-			browser.click(FILTER_ANCHOR_SELECTOR);
-			browser.click(FILTER_ITEM_ANCHOR_SELECTOR);
 
-			while (!browser.getHTML().contains(SCROLL_STOP_VALUE)
+			browser.click(FILTER_ITEM_ANCHOR_SELECTOR);
+			try {
+				// wait for results to load after text filter
+				Thread.sleep(800L);
+			} catch (InterruptedException ex) {
+				// not interested
+			}
+
+			while (scrollCount < SCROLL_STOP_VALUE
 					&& browser.countElements(RESULT_ANCHOR_SELECTOR) < query.getMaxResults()) {
 				browser.scrollToBottom();
+				scrollCount++;
 				try {
 					// wait for results to load after scroll
-					Thread.sleep(500L);
+					Thread.sleep(800L);
 				} catch (InterruptedException ex) {
 					// not interested
 				}
 			}
 			List<String> results = browser.getTexts(RESULT_ANCHOR_SELECTOR);
-			
+
 			browser.close();
 
 			return results;
@@ -53,26 +60,4 @@ public class TumblrFetcher implements Fetcher {
 		return TUMBLR_URL + "/search/" + HTTPUtils.urlEncode(query.getKeyword());
 	}
 
-	public static void main(String[] args) throws FetchException {
-		Query q = new Query();
-		TumblrFetcher tf = new TumblrFetcher();
-		
-		q.setKeyword("text");
-		q.setMaxResults(100L);
-		
-		List<String> results;
-		
-		try {
-			 results = tf.fetch(q);
-		} catch (Throwable t) {
-			throw new FetchException(t);
-		}
-		
-		for (String result : results)
-		{
-		    System.out.println("------" + result);
-		}
-		
-		
-	}
 }
